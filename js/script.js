9e68429c05abcf20bfa1880c27ff44a58bdc06eb
Norhan Adel <norhanadel9917@gmail.com>
2023-10-11 12:13:35 +0300

    upload project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(window).on('load', function () {
+
+  $('.loader').fadeOut(1000);
+new WOW().init();
+
+});
+new WOW().init();
+  // ////////////
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
